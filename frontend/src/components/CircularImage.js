8e3ac23aace28d767464a66360981b1aaa377147
zeeshangondal <zeeshangondal0007@gmail.com>
2023-11-24 22:39:19 +0500

    color borders fixed

--- frontend/src/components/CircularImage.js
+++ frontend/src/components/CircularImage.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
-const CircularImage = ({ size, glassType, label ,onClick, textureValue,clickedTextureValue}) => {
-  let isClicked=textureValue==clickedTextureValue
+const CircularImage = ({ size, glassType, label, onClick, textureValue, clickedTextureValue }) => {
+  let isClicked = textureValue === clickedTextureValue;
   const [imagePath, setImagePath] = useState('');
 
   useEffect(() => {
     import(`../GlassPictures/${glassType}.jpg`)
-      .then(image => setImagePath(image.default))
-      .catch(error => console.error('Error loading image:', error));
+      .then((image) => setImagePath(image.default))
+      .catch((error) => console.error('Error loading image:', error));
   }, [glassType]);
 
   const imageStyle = {
@@ -17,20 +17,37 @@ const CircularImage = ({ size, glassType, label ,onClick, textureValue,clickedTe
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     backgroundImage: `url(${imagePath})`,
-    boxShadow: isClicked ? '0 0 0 5px white, 0 0 0 6px black' : 'none',
-    cursor:'pointer'
+    boxShadow: isClicked
+      ? '0 0 0 5px white, 0 0 0 6px black, 0 0 0 10px rgba(255, 255, 255, 0.5), 0 2px 4px rgba(0, 0, 0, 0.1)' // Adjust the shadow size and color
+      : '0 4px 8px rgba(0, 0, 0, 0.3), 0 2px 4px rgba(0, 0, 0, 0.1)', // Adjust the shadow size and color
+    cursor: 'pointer',
+    position: 'relative',
+  };
+
+  const whiteBorderStyle = {
+    position: 'absolute',
+    content: '',
+    width: 'calc(100% + 10px)', // Adjust the border size as needed
+    height: 'calc(100% + 10px)', // Adjust the border size as needed
+    borderRadius: '50%',
+    border: '5px solid white', // Adjust the border size as needed
+    boxSizing: 'border-box',
+    top: '-5px',
+    left: '-5px',
   };
 
   const labelStyle = {
     textAlign: 'center',
     marginTop: '2px', // Adjust as needed
-    fontWeight:"normal",
-    fontSize:'12px'
+    fontWeight: 'normal',
+    fontSize: '12px',
   };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin:'1vh' }}>
-      <div style={imageStyle} onClick={onClick}/>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '1vh' }}>
+      <div style={imageStyle} onClick={onClick}>
+        <div style={whiteBorderStyle}></div>
+      </div>
       <small style={labelStyle}>{label}</small>
     </div>
   );
--- frontend/src/components/ColorfulCircle.js
+++ frontend/src/components/ColorfulCircle.js
@@ -1,19 +1,37 @@
-import React from 'react';
+const ColorfulCircle = ({ size, color, chosenColor, setColor }) => {
+  let isClicked = chosenColor === color;
 
-const ColorfulCircle = ({ size, color, chosenColor , setColor}) => {
-  let isClicked=chosenColor==color
   const style = {
     width: size,
     height: size,
     backgroundColor: color,
     borderRadius: '50%',
-    boxShadow: isClicked ? '0 0 0 5px white, 0 0 0 6px black' : 'none',
+    boxShadow: isClicked
+      ? '0 0 0 10px white, 0 0 0 11px rgba(0, 0, 0, 0.4)' // Adjust the shadow size and color
+      : '0 9px 12px rgba(0, 0, 0, 0.3)', // Adjust the shadow size and color
     display: 'inline-block',
-    margin:"1vh",
-    cursor:"pointer"
+    margin: '1vh',
+    cursor: 'pointer',
+    position: 'relative',
   };
 
-  return <div   style={style}  onClick={()=>setColor(color)}/>;
+  const whiteBorderStyle = {
+    position: 'absolute',
+    content: '',
+    width: 'calc(100% + 8px)', // Adjust the border size as needed
+    height: 'calc(100% + 8px)', // Adjust the border size as needed
+    borderRadius: '50%',
+    border: '4px solid white', // Adjust the border size as needed
+    boxSizing: 'border-box',
+    top: '-4px',
+    left: '-4px',
+  };
+
+  return (
+    <div style={style} onClick={() => setColor(color)}>
+      <div style={whiteBorderStyle}></div>
+    </div>
+  );
 };
 
 export default ColorfulCircle;
